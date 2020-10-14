92904f8aa7d6a5c052016fcce73a6584c3b9830f
Chow <kahoo.chow@syccuxas01.pcc.edu>
2020-10-13 21:38:58 -0700

    still buggy

--- lab2/linkedlist.cpp
+++ lab2/linkedlist.cpp
@@ -21,10 +21,20 @@ LinkedList::~LinkedList()
 void LinkedList::add(char ch)
 {
 	Node * newNode = new Node();
+	Node * last = nullptr;
 	newNode->data = ch;
-	newNode->next = head;
-	head = newNode;
-	
+	newNode->next = nullptr;
+	if(head == nullptr)
+	{
+		head = newNode;
+		return;
+	}
+	last = head;
+	while(last->next != nullptr)
+	{
+		last = last->next;
+	}
+	last->next = newNode;
 }
 
 bool LinkedList::find(char ch)
