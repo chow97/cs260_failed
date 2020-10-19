6428858b0a7d6b18ca878c711cbaf730a6d7c29d
Chow <chow@Chows-MacBook-Pro.local>
2020-10-19 23:10:56 +0800

    adding cpp

--- project1/songLibrary.h
+++ project1/songLibrary.h
@@ -1,2 +1,29 @@
 #ifndef SONGLIBRARY_H
+#define SONGLIBRARY_H
 
+#include <cstring>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class SongList 
+{
+    public:
+        SongList();
+        ~SongList();
+        void addSong(char title[], char artist[], char album[], char duration[]);
+        void removeSong();
+        void numViews();
+        void displayAllArtists();
+        void displayAllSongs();
+
+    private:
+        struct Node 
+        {
+            char data;
+            Node * next;
+            Node * prev;
+        };
+        Node * head;
+};
