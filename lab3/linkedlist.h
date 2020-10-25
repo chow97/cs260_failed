2cb81476cd0465dec6c0fb55b77e8a63e8431834
Chow <kahoo.chow@syccuxas01.pcc.edu>
2020-10-24 22:40:12 -0700

    adding lab3

---
+++ lab3/linkedlist.h
@@ -0,0 +1,19 @@
+#ifndef _LINKED_LIST_
+#define _LINKED_LIST_
+
+#include <ostream>
+
+class LinkedList
+{
+public:
+	LinkedList();
+	~LinkedList();
+
+	void add(char ch);
+	bool find(char ch);
+	bool del(char ch);
+
+	friend std::ostream& operator<<(std::ostream& out, LinkedList& list);
+};
+
+#endif // _LINKED_LIST_
